eeb8596a076b56489d2b709db63a46fdf14b6ccb
= <=>
2023-01-11 13:12:52 +0530

    prog1

--- package_programs/prog1.java
+++ package_programs/prog1.java
@@ -1,8 +1,12 @@
 // Java Program to Print an Integer (Entered by the User)
+import java.util.*;
 class prog1
 {
     public static void main(String[] args)
     {
-          
+        Scanner input = new Scanner(System.in);
+        
+        int number1 = input.nextInt();
+        System.out.println("Number is : "+number1);
     }
 }
