2dd9cc7cfcd697c1426b0b7bb5b3f4786cce73c3
= <=>
2023-01-11 13:31:09 +0530

    prog5 half

---
+++ package_programs/prog3.java
@@ -0,0 +1,13 @@
+// Java Program to Multiply two Floating Point Numbers
+import java.util.*;
+class prog3
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        float number1 = input.nextFloat();
+        float number2 = input.nextFloat();
+        System.out.println("the number Multiplication is : "+(number1*number2));
+    }
+}
---
+++ package_programs/prog5.java
@@ -0,0 +1,12 @@
+// Java Program to Compute Quotient and Remainder
+import java.util.*;
+class prog4
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        int Ascii = (int) ch;
+        System.out.println("the Ascii is : "+Ascii);
+    }
+}
