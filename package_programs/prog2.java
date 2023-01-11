f1e27c26449dacfe379c530544afb43a2adaedbc
= <=>
2023-01-11 13:18:42 +0530

    prog2

---
+++ package_programs/prog2.java
@@ -0,0 +1,13 @@
+// Java Program to Add Two Integers
+import java.util.*;
+class prog2
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        System.out.println("the number Addtion is : "+(number1+number2));
+    }
+}
