5625f9cd2ce6b4be0bc56996b78eb3682460e3b6
= <=>
2023-01-13 12:24:35 +0530

    new

--- package_After10/prog12.java
+++ package_After10/prog12.java
@@ -1,5 +1,19 @@
 package package_After10;
 
+// Java Program to Check Whether a Number is Positive or Negative
 public class prog12 {
-    
+    public prog12(double num){
+        if(num > 0){
+            System.out.println(" The number is positive");
+        }
+        else if(num < 0){
+            System.out.println(" The number is negetive");
+        }
+        else{
+            System.out.println(" The number is Zero");
+        }
+    }
+    public static void main(String[] args) {
+        prog12 obj = new prog12(-10.100);        
+    }
 }
